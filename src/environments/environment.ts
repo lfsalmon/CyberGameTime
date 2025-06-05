e911fdcc5728b8d350933034bb161e748f8e2032
Luis Fernando Salmon <67918046+lfsalmon@users.noreply.github.com>
2025-06-05 13:50:28 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: 'http://192.168.1.200/cybergametime_api/' // cámbialo cuando tengas dominio/productivo
+  apiUrl: 'http://192.168.100.200/cybergametime_api/' // cámbialo cuando tengas dominio/productivo
 };
